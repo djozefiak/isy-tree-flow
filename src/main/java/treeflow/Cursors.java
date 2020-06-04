92afe2ad645704eb85768885d3724087eafcb33d
David Jozefiak <35257958+djozefiak@users.noreply.github.com>
2020-06-04 22:12:24 +0200

    Move cursor graphics into separate folder

--- src/main/java/treeflow/Cursors.java
+++ src/main/java/treeflow/Cursors.java
@@ -6,21 +6,21 @@ import javafx.scene.image.Image;
 public class Cursors {
 
     public static final ImageCursor UP = new ImageCursor(
-            new Image(Cursors.class.getResource("/graphics/up.png").toExternalForm()),
+            new Image(Cursors.class.getResource("/cursors/up.png").toExternalForm()),
             255, 5);
     public static final ImageCursor RIGHT = new ImageCursor(
-            new Image(Cursors.class.getResource("/graphics/right.png").toExternalForm()),
+            new Image(Cursors.class.getResource("/cursors/right.png").toExternalForm()),
             506, 255);
     public static final ImageCursor DOWN = new ImageCursor(
-            new Image(Cursors.class.getResource("/graphics/down.png").toExternalForm()),
+            new Image(Cursors.class.getResource("/cursors/down.png").toExternalForm()),
             255, 506);
     public static final ImageCursor LEFT = new ImageCursor(
-            new Image(Cursors.class.getResource("/graphics/left.png").toExternalForm()),
+            new Image(Cursors.class.getResource("/cursors/left.png").toExternalForm()),
             5, 255);
     public static final ImageCursor HORIZONTAL = new ImageCursor(
-            new Image(Cursors.class.getResource("/graphics/horizontal.png").toExternalForm()),
+            new Image(Cursors.class.getResource("/cursors/horizontal.png").toExternalForm()),
             255, 255);
     public static final ImageCursor VERTICAL = new ImageCursor(
-            new Image(Cursors.class.getResource("/graphics/vertical.png").toExternalForm()),
+            new Image(Cursors.class.getResource("/cursors/vertical.png").toExternalForm()),
             255, 255);
 }
